bbc16edae2afd87898df384c2e7653d6790834d6
Andreas Maier <andreas.maier@ars.de>
2022-02-18 17:51:36 +0100

    Füge weitere Methode /hallo mit RequestParam hinzu

--- src/main/java/de/ars/restSchulung/todos/boundary/HelloWorldController.java
+++ src/main/java/de/ars/restSchulung/todos/boundary/HelloWorldController.java
@@ -3,6 +3,7 @@ package de.ars.restSchulung.todos.boundary;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,7 +12,12 @@ public class HelloWorldController {
     @GetMapping(value = "/hello", produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
     public String sayHello() {
-        return "<h1>Hallo  Welt</h1>";
+        return "<h1>Hello  World</h1>";
     }
 
+    @GetMapping(value = "/hallo", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String sagHallo(@RequestParam(defaultValue = "") String name) {
+        return "Hallo " + name;
+    }
 }
