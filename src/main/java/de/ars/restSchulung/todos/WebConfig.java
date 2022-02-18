2915616b0330c5527457a1c1602ec142e32ad376
Andreas Maier <andreas.maier@ars.de>
2022-02-18 17:58:34 +0100

    Füge index.html und WebConfig.java hinzu

---
+++ src/main/java/de/ars/restSchulung/todos/WebConfig.java
@@ -0,0 +1,14 @@
+package de.ars.restSchulung.todos;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static-files/");
+    }
+}
