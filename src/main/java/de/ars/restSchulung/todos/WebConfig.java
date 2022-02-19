c2fce7636c76dfcf1565ed86d96db3f7fb86c7b1
Andreas Maier <andreas.maier@ars.de>
2022-02-19 16:02:08 +0100

    Füge jstl, tomcat-embed-jasper und jsp hinzu
    webapp/jsp Ordner unter main

--- src/main/java/de/ars/restSchulung/todos/WebConfig.java
+++ src/main/java/de/ars/restSchulung/todos/WebConfig.java
@@ -1,9 +1,13 @@
 package de.ars.restSchulung.todos;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -16,5 +20,14 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("redirect:/index.html");
+    }
+
+    @Bean
+    public ViewResolver internalViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
     }
 }
--- src/main/java/de/ars/restSchulung/todos/boundary/TodoController.java
+++ src/main/java/de/ars/restSchulung/todos/boundary/TodoController.java
@@ -1,14 +1,16 @@
 package de.ars.restSchulung.todos.boundary;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class TodoController {
     @GetMapping(value = "/todos")
-    @ResponseBody
-    public Todo findAll() {
-        return new Todo("Spring Boot lernen", 5);
+    public String findAll(Model model) {
+        Todo todo = new Todo("Spring Boot lernen", 5);
+        model.addAttribute("todo", todo);
+        return "ausgabe";
     }
 }
